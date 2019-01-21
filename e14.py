bce84f63f2f2726557cd2d3b35b3e8429b07cba6
Karumishra <42409280+Karumishra@users.noreply.github.com>
2019-01-21 16:19:07 +0530

    Add files via upload

---
+++ e14.py
@@ -0,0 +1,13 @@
+name = input('Enter your name ')
+age =  input('Enter your age ')
+def pl():
+    usn = input('Enter your usn ')
+    print('Your name is '+name+' and your age is '+age)
+pl()
+
+
+
+
+
+
+
