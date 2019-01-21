7b9e76bfc9745a6a38b2b62a7f28df0a61fe19ef
Karumishra <42409280+Karumishra@users.noreply.github.com>
2019-01-21 16:24:12 +0530

    Add files via upload

---
+++ e22.py
@@ -0,0 +1,16 @@
+def addnum(*args):
+    total=0
+    for x in args:
+        total+=x
+
+    print(total)
+addnum(43)
+addnum(1,2,3,4,6)
+addnum(1,2,3,4,5,6,7,8,9,0)    #flexiblearguments
+
+
+
+
+
+
+
