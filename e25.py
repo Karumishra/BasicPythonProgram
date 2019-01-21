3648d1b458ab3415df0f5abca64bd5f42a51fe03
Karumishra <42409280+Karumishra@users.noreply.github.com>
2019-01-21 16:25:46 +0530

    Add files via upload

---
+++ e25.py
@@ -0,0 +1,20 @@
+class Ex:
+    count = 3
+    def op(self):
+        print('Hey guys!')
+        self.count-=1
+    def pl(self):
+        print(str(self.count)+" remaining")
+we = Ex()    #creatingobject
+we.op()      #callingfunc
+we.pl()
+
+
+
+
+
+
+
+
+
+
