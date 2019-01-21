37837ef443e97f55d0ae2e39dbe362cd1e8c856e
Karumishra <42409280+Karumishra@users.noreply.github.com>
2019-01-21 16:34:06 +0530

    Add files via upload

---
+++ e30.py
@@ -0,0 +1,8 @@
+class JK:
+
+    def __init__(self,x):                           #justlikeconfstructoric++
+        self.usn = x
+    def yoyo(self):
+        print(self.usn)
+jk = JK(10)
+jk.yoyo()
